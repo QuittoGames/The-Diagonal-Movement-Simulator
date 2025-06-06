d637fbfa7c189744fcfe5a30fe4502c981537d85
Quitto <brunamoreiraquitto@gmail.com>
2025-06-06 09:55:27 -0300

    1.0v Add struture in code

--- index.py
+++ index.py
@@ -1,7 +1,19 @@
 import pygame
+from tool import tool
+from data import data
+import asyncio
+
+#Iniliza Uma data local para haver modificaçoes no atributo
+data_local = data()
 
 def Start():
     pass
 
+def main():
+    asyncio.create_task(tool.add_path_modules())
+    asyncio.create_task(tool.verify_modules())
+    return
+
 if __name__ == "__main__":
+    main()
     Start()
--- tool.py
+++ tool.py
@@ -1,6 +1,9 @@
 import os
 import platform
 from dataclasses import dataclass
+import subprocess
+from data import data
+import sys
 
 @dataclass
 class tool:
@@ -9,4 +12,25 @@ class tool:
             os.system('cls')
         else:
             os.system('clear')
-    #Project created successfully!
+
+    async def verify_modules():
+        try:
+            # #Uso Do modules por txt
+            req_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "modules", "requirements.txt"))
+            subprocess.run([sys.executable, "-m", "pip", "install", "-r", req_path], check=True)
+        
+        except Exception as E:
+            print(f"Erro Na Verificaçao De Modulos, Erro: {E}")
+            return
+        
+    async def add_path_modules(data_local:data):
+        if data.modules_local == None:return
+        try:
+            for i in data.modules_local:
+                sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), i)))
+                if data_local.Debug:print(f"Module_local: {i}")
+            return
+        except Exception as E:
+            print(f"Erro Al Adicionar Os Caminhos Brutos, Erro: {E}")
+            return
+   
